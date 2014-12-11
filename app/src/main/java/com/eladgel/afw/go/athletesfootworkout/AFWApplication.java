e6a22f873018983955ee0837bfa0cb54d36f2911
Elad Gelman <elad@appmyday.com>
2014-12-11 13:09:16 +0200

    commit

---
+++ app/src/main/java/com/eladgel/afw/go/athletesfootworkout/AFWApplication.java
@@ -0,0 +1,9 @@
+package com.eladgel.afw.go.athletesfootworkout;
+
+import android.app.Application;
+
+/**
+ * Created by eladgelman on 12/11/14.
+ */
+public class AFWApplication extends Application {
+}
